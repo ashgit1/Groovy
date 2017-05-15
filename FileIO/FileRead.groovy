04d5c3d37a11b2f19def69b6746c3444117d24df
Ashish Gupta <adoreashish@gmail.com>
2017-05-15 10:30:10 +0530

    Updating the directory name from the current path

--- FileIO/FileRead.groovy
+++ FileIO/FileRead.groovy
@@ -2,7 +2,7 @@ import java.io.File
 
 class FileRead { 
    static void main(String[] args) { 
-      File file = new File("/Users/a0k00ee/Documents/Ashu/learn/groovy/install-steps.txt"); 
+      File file = new File("./../install-steps.txt"); 
 		println file.text;	         
    } 
 }
--- FileIO/FileWrite.groovy
+++ FileIO/FileWrite.groovy
@@ -2,7 +2,7 @@ import java.io.File;
 
 class FileWrite{
 	static void main(String[] args){
-		new File("/Users/a0k00ee/Documents/Ashu/learn/groovy/FileIO/", 
+		new File("./", 
 			"write.txt").withWriter('utf-8') { 
 				writer -> writer.writeLine 'Hello Groovy'	
 			}
